7ecce455165846babd8abee1f9756d7c669da4f4
Nazia Nafis <81024656+nazianafis@users.noreply.github.com>
2023-03-24 09:57:32 +0530

    Create cohen.py

---
+++ preprocessing/cohen.py
@@ -0,0 +1,24 @@
+def cohen_kappa(ann1, ann2):
+    """Computes Cohen kappa for pair-wise annotators.
+    :param ann1: annotations provided by first annotator
+    :type ann1: list
+    :param ann2: annotations provided by second annotator
+    :type ann2: list
+    :return: Cohen kappa statistic
+    :rtype: float
+    """
+    count = 0
+    for an1, an2 in zip(ann1, ann2):
+        if an1 == an2:
+            count += 1
+    A = count / len(ann1)  # observed agreement A (Po)
+
+    uniq = set(ann1 + ann2)
+    E = 0  # expected agreement E (Pe)
+    for item in uniq:
+        cnt1 = ann1.count(item)
+        cnt2 = ann2.count(item)
+        count = ((cnt1 / len(ann1)) * (cnt2 / len(ann2)))
+        E += count
+
+    return round((A - E) / (1 - E), 4)
